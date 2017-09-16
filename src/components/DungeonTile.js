687156f98d9d6a9d4f63e30486fe3322e501474c
ReeceLangerock <ReeceLangerock@gmail.com>
2017-09-15 22:31:31 -0500

    add dungeonTile component, including styling and shouldComponentUpdate

--- src/components/DungeonTile.js
+++ src/components/DungeonTile.js
@@ -1,13 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-// const FooterStyled = styled.div``;
+const Tile = styled.div`
+  width: 12px;
+  height: 12px;
 
-// const I = styled.i``;
+  background: ${props => {
+    if (props.on) {
+      return "green";
+    } else if (props.player) {
+      return "red";
+    } else {
+      return "grey";
+    }
+  }};
+`;
 
 export class DungeonTile extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return this.props.on !== nextProps.on;
+  }
   render() {
-    return <div/>;
+    return <Tile id={this.props.index} player={this.props.player} on={this.props.on} />;
   }
 }
 export default DungeonTile;
