7598f41bba782e26b5162d1ce09f67830d2a0bb0
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-02 22:16:28 -0500

    add conditional background based on tile type, fixed shouldComponentUpdate

--- src/components/DungeonTile.js
+++ src/components/DungeonTile.js
@@ -1,27 +1,47 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 const Tile = styled.div`
   width: 12px;
   height: 12px;
-
-  background: ${props => {
+  font-size: 7px;
+  z-index: ${props => {
     if (props.on) {
-      return "green";
-    } else if (props.player) {
-      return "red";
     } else {
-      return "grey";
+      return 5
     }
   }};
-`;
+  background: ${props => {
+    if (props.player) {
+      return 'blue'
+    } else if (props.enemy) {
+      return 'red'
+    } else if (props.weapon) {
+      return 'yellow'
+    } else if (props.portal) {
+      return 'purple'
+    } else if (props.on) {
+      return 'green'
+    } else {
+      return 'grey'
+    }
+  }};
+`
 
 export class DungeonTile extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return this.props.on !== nextProps.on;
+  shouldComponentUpdate (nextProps) {
+    return (
+      this.props.on !== nextProps.on ||
+      this.props.player !== nextProps.player ||
+      this.props.enemy !== nextProps.enemy ||
+      this.props.portal !== nextProps.portal ||
+      this.props.weapon !== nextProps.weapon
+    )
   }
-  render() {
-    return <Tile id={this.props.index} player={this.props.player} on={this.props.on} />;
+  render () {
+    console.log('render count')
+
+    return <Tile id={this.props.index} player={this.props.player} on={this.props.on} enemy={this.props.enemy} weapon={this.props.weapon} portal={this.props.portal} />
   }
 }
-export default DungeonTile;
+export default DungeonTile
