2d8e511d6d2ab2d65dad9f3eac10535bd38ddc6d
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-10 17:13:25 -0500

    tweak reducers for health and xp, change dungeonEntities weapon format

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -3,7 +3,7 @@ export var playerReducer = (
     position: [],
     health: 100,
     maxHealth: 100,
-    weapon: 'a Herring!',
+    weapon: "a Herring!",
     weaponDamage: 5,
     exp: 0,
     expNeeded: 100,
@@ -12,19 +12,38 @@ export var playerReducer = (
   action
 ) => {
   switch (action.type) {
-    case 'ADD_HEALTH':
-      let newHealth = action.health + state.health
-      let newMaxHealth = newHealth > state.maxHealth ? newHealth : state.maxHealth
-      return { ...state, health: newHealth, maxHealth: newMaxHealth }
+    case "ADD_HEALTH":
+      let newHealth = action.health + state.health;
+      let newMaxHealth =
+        newHealth > state.maxHealth ? newHealth : state.maxHealth;
+      return { ...state, health: newHealth, maxHealth: newMaxHealth };
 
-    case 'REMOVE_HEALTH':
-      return { ...state, health: state.health - action.health }
-    case 'GAIN_EXP':
-      return { ...state, exp: state.exp - action.exp }
-    case 'EQUIP_NEW_WEAPON':
-      return { ...state, weapon: action.weapon.weaponName, weaponDamage: state.weaponDamage + action.weapon.weaponDamage }
+    case "REMOVE_HEALTH":
+      return { ...state, health: state.health - action.health };
+    case "GAIN_EXP":
+      let newExp = action.exp + state.exp;
+      let newExpNeeded = state.expNeeded;
+      let newLevel = state.level;
+      if (newExp >= state.expNeeded) {
+        newExpNeeded = state.expNeeded + 100;
+        newExp = newExp - state.expNeeded;
+        newLevel++;
+      }
+
+      return {
+        ...state,
+        exp: newExp,
+        expNeeded: newExpNeeded,
+        level: newLevel
+      };
+    case "EQUIP_NEW_WEAPON":
+      return {
+        ...state,
+        weapon: action.weapon.weaponName,
+        weaponDamage: state.weaponDamage + action.weapon.weaponDamage
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
