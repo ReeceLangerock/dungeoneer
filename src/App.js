bf4fc247e618d4258fefd7a5d2106e3245a2a745
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-14 23:05:21 -0500

    add custom font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import './App.css'
-import styled from 'styled-components'
+import styled, { injectGlobal } from 'styled-components'
+
 
 import PlayerInfoContainer from './components/PlayerInfoContainer'
 import Header from './components/Header'
 import Dungeon from './components/Dungeon'
+import Toggle from './components/Toggle'
 
 import { Provider } from 'react-redux'
 import store, { history } from './redux/store/store'
+
+import OptimusPrinceps from './OptimusPrinceps.ttf'
+
+injectGlobal`
+  @font-face {
+    font-family: 'OptimusPrinceps';
+    src: url('${OptimusPrinceps}');
+  }
+  
+`
 
 class App extends Component {
   render () {
@@ -15,6 +27,7 @@ class App extends Component {
       <Provider store={store}>
 
         <Container className='App'>
+          <Toggle/>
           <Header />
           <Dungeon />
           <PlayerInfoContainer />
@@ -34,5 +47,7 @@ max-width: 100vw;
 min-height: 100vh;
 overflow: hidden;
 position: relative;
-background: darkgrey;
+background: #191919;
+
+// background: darkgrey;
 `
