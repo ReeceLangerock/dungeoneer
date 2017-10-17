010825a111bc5ac4391e3a7e9c9853790cd85022
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-16 22:28:58 -0500

    cleanup and remove unused items

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,9 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest-enzyme';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import 'jest-enzyme'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 
-global.requestAnimationFrame = function(callback) {
-    setTimeout(callback, 0);
-  };
+global.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0)
+}
