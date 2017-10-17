2e3fc08c07680e323557cb405af90feef063842a
ReeceLangerock <ReeceLangerock@gmail.com>
2017-10-16 22:25:57 -0500

    move movement handling out of component, add handling for resizing dungeon on windo resize, correct improper state changes

--- src/components/Dungeon.js
+++ src/components/Dungeon.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { injectGlobal } from 'styled-components'
+import styled from 'styled-components'
 
 import DungeonTile from './DungeonTile'
 import DungeonMaster from './../javascript/dungeon-master.js'
@@ -8,34 +8,53 @@ import * as actions from './../redux/actions/actions'
 import GameMessage from './GameMessage'
 
 const DungeonContainer = styled.div`
-  width: 960px;
-  // height: 552px;
+  width: ${props => props.width + 'px'};
+  height: ${props => props.height + 'px'};
+  // height: 452px;
   background-color: grey;
   margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
+
 `
 
 export class Dungeon extends React.Component {
   constructor (props) {
     super(props)
-    this.handleKeyDown = this.handleKeyDown.bind(this)
+
+    this.handleMove = this.handleMove.bind(this)
+    this.setWindowSize = this.setWindowSize.bind(this)
     this.state = {
-      dungeon: false,
-      lightsOut: false
+      dungeon: false
     }
+  }
+  setWindowSize () {
+    let maxWidth = Math.min(window.innerWidth - 12, 1200)
+    maxWidth = maxWidth - maxWidth % 24
+    let maxHeight = Math.min(window.innerHeight - 240, 600)
+    maxHeight = maxHeight - maxHeight % 24
+
+    this.setState(
+      {
+        width: maxWidth,
+        height: maxHeight
+      },
+      this.centerCamera(this.state.playerPosition, maxWidth)
+    )
   }
 
   // INITIALIZE DUNGEONS
   componentWillMount () {
+    window.addEventListener('resize', this.setWindowSize)
     this.initialize()
+    this.setWindowSize()
   }
   componentWillReceiveProps (newProps) {
-    console.log(newProps.gameStatus)
     if (newProps.gameStatus === 'init') {
       this.initialize()
     }
   }
+
   initialize () {
     const dungeonMaster = new DungeonMaster()
     dungeonMaster
@@ -76,61 +95,31 @@ export class Dungeon extends React.Component {
   }
 
   // MOVEMENT HANDLING
-  handleKeyDown (key) {
-    // W - 87; A - 65; S - 83; D - 68;
-
+  handleMove (vector, direction) {
     var currentPlayerPosition = Object.assign([], this.state.playerPosition)
-
-    switch (key.keyCode) {
-      case (38, 87):
-        currentPlayerPosition[0] = currentPlayerPosition[0] - 1
-        this.handleMove(currentPlayerPosition, 'north')
-        break
-
-      case (39, 68):
-        currentPlayerPosition[1] = this.state.playerPosition[1] + 1
-        this.handleMove(currentPlayerPosition, 'east')
-        break
-      case (40, 83):
-        currentPlayerPosition[0] = this.state.playerPosition[0] + 1
-        this.handleMove(currentPlayerPosition, 'south')
-        break
-      case (37, 65):
-        currentPlayerPosition[1] = this.state.playerPosition[1] - 1
-        this.handleMove(currentPlayerPosition, 'west')
-        break
-
-      case 69:
-        this.handlePortal()
-      default:
-    }
-  }
-
-  handleMove (newPosition, direction) {
+    let newPosition = []
+    newPosition[0] = currentPlayerPosition[0] + vector[0]
+    newPosition[1] = currentPlayerPosition[1] + vector[1]
     let whosThere = this.findEntityAtNewPosition(newPosition)
     switch (whosThere.entity) {
       case 'floor':
         this.moveCamera(direction)
-
         this.setState({
           playerPosition: newPosition
         })
         break
       case 'enemy':
       case 'boss':
-        // if combat doesn;t result in player killing enemy, return from handleMove without doing anything
         if (whosThere.entity === 'boss') {
           whosThere = this.state.boss
         }
         if (!this.handleCombat(whosThere, newPosition)) {
           return
         }
-
         break
 
       case 'weapon':
         this.handleWeapon(whosThere, newPosition)
-
         break
       case 'health':
         this.handleHealthPickup(whosThere, newPosition)
@@ -139,10 +128,8 @@ export class Dungeon extends React.Component {
       case 'portal':
         this.handlePortal()
         return
-        break
       default:
         return
-        break
     }
 
     this.setState({
@@ -157,7 +144,7 @@ export class Dungeon extends React.Component {
 
     if (enemy.health > playerAttack) {
       if (enemyAttack > this.props.health) {
-        this.props.updateGameStatus('dead')
+        this.handleGameOver('dead')
         return false
       }
       if (enemy.entity === 'boss') {
@@ -166,33 +153,43 @@ export class Dungeon extends React.Component {
           boss: enemy
         })
       } else {
-        this.state.dungeon[enemyLoc[0]][enemyLoc[1]].health = this.state.dungeon[enemyLoc[0]][enemyLoc[1]].health - playerAttack
+        const dungeon = this.state.dungeon
+        dungeon[enemyLoc[0]][enemyLoc[1]].health = dungeon[enemyLoc[0]][enemyLoc[1]].health - playerAttack
+
+        this.setState({
+          dungeon
+        })
       }
       this.props.removeHealth(enemyAttack)
     } else if (playerAttack >= enemy.health) {
       if (enemy.entity === 'boss') {
         this.handleGameOver('victory')
       }
-      let expGained = enemy.exp
-      this.props.gainExp(expGained)
+      this.props.gainExp(enemy.exp)
       this.props.enemySlain()
-      this.state.dungeon[enemyLoc[0]][enemyLoc[1]] = {
-        entity: 'floor'
-      }
+      const dungeon = this.state.dungeon
+      dungeon[enemyLoc[0]][enemyLoc[1]].entity = 'floor'
+      this.setState({
+        dungeon
+      })
+
       return true
     }
     return false
   }
 
   handleWeapon (weapon, weaponLoc) {
-    this.state.dungeon[weaponLoc[0]][weaponLoc[1]] = {
-      entity: 'floor'
-    }
+    const dungeon = this.state.dungeon
+    dungeon[weaponLoc[0]][weaponLoc[1]].entity = 'floor'
+    this.setState({
+      dungeon
+    })
+
     this.props.equipNewWeapon(weapon)
   }
 
   handlePortal () {
-    let dungeonLevel = this.props.dungeonLevel + 1
+    let dungeonLevel = this.props.currentDungeonLevel + 1
     if (dungeonLevel < 5) {
       let newStart = this.state[`startingPosition${dungeonLevel}`]
       this.setState(
@@ -203,21 +200,22 @@ export class Dungeon extends React.Component {
         this.centerCamera(newStart)
       )
       this.props.updateDungeonLevel(dungeonLevel)
-    } else {
-      // handle game over
     }
   }
 
   handleHealthPickup (health, healthLoc) {
-    this.state.dungeon[healthLoc[0]][healthLoc[1]] = {
-      entity: 'floor'
-    }
+    const dungeon = this.state.dungeon
+    dungeon[healthLoc[0]][healthLoc[1]].entity = 'floor'
+    this.setState({
+      dungeon
+    })
+
     this.props.addHealth(health.health)
   }
 
   handleGameOver (status) {
-    this.props.updateDungeonLevel(1)
     this.props.updateGameStatus(status)
+    this.props.resetGame()
   }
   findEntityAtNewPosition (newPosition) {
     if (typeof this.state.dungeon[newPosition[0]][newPosition[1]] === 'object') {
@@ -259,6 +257,8 @@ export class Dungeon extends React.Component {
           lastVisibleColumn = this.state.lastVisibleColumn - 1
         }
         break
+      default:
+        break
     }
     this.setState({
       firstVisibleRow,
@@ -268,27 +268,26 @@ export class Dungeon extends React.Component {
     })
   }
 
-  centerCamera (newStart) {
-    let firstVisibleRow = newStart[0] - 23 > 0 ? newStart[0] - 23 : 0
-    let LastVisibleRow = newStart[0] + 23 < 100 ? newStart[0] + 23 : 100
-    let firstVisibleColumn = newStart[1] - 40 > 0 ? newStart[1] - 40 : 0
-    let lastVisibleColumn = firstVisibleColumn + 80
+  centerCamera (newStart = [50, 50], maxWidth = this.state.width, maxHeight = this.state.height) {
+    let widthDiff = Math.floor(maxWidth / 12 / 2)
+    let heightDiff = Math.floor(maxHeight / 12 / 2)
+    let firstVisibleRow = newStart[0] - heightDiff > 0 ? newStart[0] - heightDiff : 0
+    let lastVisibleRow = newStart[0] + heightDiff < 100 ? newStart[0] + heightDiff : 100
+    let firstVisibleColumn = newStart[1] - widthDiff > 0 ? newStart[1] - widthDiff : 0
+    let lastVisibleColumn = firstVisibleColumn + maxWidth / 12
     this.setState({
       firstVisibleRow: firstVisibleRow,
-      lastVisibleRow: LastVisibleRow,
+      lastVisibleRow: lastVisibleRow,
       firstVisibleColumn: firstVisibleColumn,
       lastVisibleColumn
     })
   }
 
   // RENDERING
-
   renderDungeonTiles () {
     if (this.state.dungeon && this.props.gameStatus === 'active') {
       let tiles = []
-      let index = 0
       let player = false
-      let wall = false
       let darkness = false
       const sight = 8
       for (let r = this.state.firstVisibleRow; r < this.state.lastVisibleRow; r++) {
@@ -307,8 +306,7 @@ export class Dungeon extends React.Component {
           } else {
             entity = 'wall'
           }
-          tiles.push(<DungeonTile index={index} row={r} key={`tile${index}`} darkness={darkness} entity={entity} player={player} />)
-          index++
+          tiles.push(<DungeonTile index={`tileR${r}C${c}`} row={r} key={`tileR${r}C${c}`} darkness={darkness} entity={entity} player={player} />)
         }
       }
 
@@ -324,7 +322,7 @@ export class Dungeon extends React.Component {
   render () {
     return (
       <div>
-        <DungeonContainer tabIndex='0' onKeyDown={this.handleKeyDown}>
+        <DungeonContainer width={this.state.width} height={this.state.height}>
           {this.renderDungeonTiles()}
         </DungeonContainer>
       </div>
@@ -332,12 +330,10 @@ export class Dungeon extends React.Component {
   }
 }
 const mapStateToProps = state => ({
-  weapon: state.playerReducer.weapon,
   weaponDamage: state.playerReducer.weaponDamage,
-  level: state.playerReducer.level,
   health: state.playerReducer.health,
   lights: state.dungeonReducer.lights,
   currentDungeonLevel: state.dungeonReducer.currentDungeonLevel,
   gameStatus: state.dungeonReducer.gameStatus
 })
-export default connect(mapStateToProps, actions)(Dungeon)
+export default connect(mapStateToProps, actions, null, { withRef: true })(Dungeon)
